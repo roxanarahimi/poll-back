8ba76f7f401ff3d167001a953f684a3263d2dd2e
Roxana <ms.roxanarahimi@gmail.com>
2026-09-10 18:01:19 +0330

    arrange

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('/test', function (Request $request) {
     return 'hooy';
 });
 
-Route::post('/mobile/otp', [\App\Http\Controllers\UserController::class, 'saveAnswer']);
-Route::post('/mobile/verify', [\App\Http\Controllers\UserController::class, 'saveAnswer']);
+Route::post('/mobile/otp', [\App\Http\Controllers\UserController::class, 'sendOtp']);
+Route::post('/mobile/verify', [\App\Http\Controllers\UserController::class, 'verifyMobile']);
 Route::get('/questions', [\App\Http\Controllers\UserController::class, 'questions']);
 Route::post('/saveAnswer', [\App\Http\Controllers\UserController::class, 'saveAnswer']);
