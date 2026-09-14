21e52c0f65612e7a8fde00b8365651ab45038eb8
Roxana <ms.roxanarahimi@gmail.com>
2026-09-14 20:02:14 +0330

    arrange

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,6 +78,7 @@ class UserController extends Controller
 
             $send = $this->sendSmsIR($sms);
             Cache::put($mobile, $code, 60);
+            return response($send,400);
             if ($send->status() === 1) {
                 return response(['message' => 'کد تایید ارسال شد.'], 200);
 
