db40baf3412bfcf63fa44c084bbc50c388a32cfa
Roxana <ms.roxanarahimi@gmail.com>
2026-09-14 19:40:27 +0330

    arrange

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -109,7 +109,7 @@ class UserController extends Controller
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS =>'{
-        "mobile": '.$mobile.',
+        "mobile": "'.$mobile.'",
         "templateId": "636094",
         "parameters": [
           {
