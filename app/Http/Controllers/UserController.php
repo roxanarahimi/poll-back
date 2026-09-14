b4376588ae8f4f0b7061cad32c138a36a16ade74
Roxana <ms.roxanarahimi@gmail.com>
2026-09-14 21:43:35 +0330

    arrange

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -190,7 +190,6 @@ class UserController extends Controller
             $mobile = $this->faToEn($request['mobile']);
             $inputCode = $this->faToEn($request['code']);
             $code = Cache::get($mobile);
-            return response([$code,$inputCode],500);
             if ($code == $inputCode) {
                 $user = User::where('mobile', $mobile)->first();
                 if (!$user) {
