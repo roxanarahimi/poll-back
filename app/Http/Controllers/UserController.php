afa054241b3554c99a010b6d7b82166a5b7b1e5b
Roxana <ms.roxanarahimi@gmail.com>
2026-09-14 19:42:29 +0330

    arrange

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -131,7 +131,6 @@ class UserController extends Controller
 
 
             $array = json_decode($result, true);
-            return response([$result,$array],500);
             if ($result) {
                 $info = [
                     "messageid" => $array['messageId'],
