df80b89757e552eae442f57ccb1be9442ec90c4d
Roxana <ms.roxanarahimi@gmail.com>
2026-09-19 22:16:28 +0330

    arrange

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,8 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+//        return str_ends_with($this->email, '@amadehlaziz.com') && $this->hasVerifiedEmail();
+        return  $this->hasVerifiedEmail();
     }
 
     public function polls(): HasMany
